33142b81159f0a5c1ee4fe6bbaa1fd2f5ea0f6f6
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-09-14 15:09:30 +0530

    initiate using vite, add dependencies, add navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./sections/Navbar";
+
+// import React from "react";
+const App = () => {
+  return (
+    <>
+      <main className="max-w-7xl mx-auto">
+        <Navbar />
+        <h1 className="text-3xl font-bold text-white-800 underline">
+          Hello Vinoth
+        </h1>
+      </main>
+    </>
+  );
+};
+
+export default App;
