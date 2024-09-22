3d415a7eda250e2dd83ab7b8e673520a4c95d387
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-09-22 21:28:14 +0530

    add demo computer animation

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,9 @@
 import { myProjects } from "../constants";
-import { useState } from "react";
+import { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Center, OrbitControls } from "@react-three/drei";
+import CanvasLoader from "../components/CanvasLoader";
+import DemoComputer from "../components/DemoComputer";
 
 const projectCount = myProjects.length;
 
@@ -51,7 +55,7 @@ const Projects = () => {
             <div className="flex items-center gap-3">
               {currentProject.tags.map((tag, index) => (
                 <div key={`${tag}_${index}`} className="tech-logo">
-                  <img src={tag.path} alt={tag.name} srcset="" />
+                  <img src={tag.path} alt={tag.name} />
                 </div>
               ))}
             </div>
@@ -87,6 +91,20 @@ const Projects = () => {
               />
             </button>
           </div>
+        </div>
+        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+          <Canvas>
+            <ambientLight intensity={Math.PI} />
+            <directionalLight position={[10, 10, 5]} />
+            <Center>
+              <Suspense fallback={<CanvasLoader />}>
+                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                  <DemoComputer texture={currentProject.texture} />
+                </group>
+              </Suspense>
+            </Center>
+            <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
+          </Canvas>
         </div>
       </div>
     </section>
