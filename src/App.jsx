8b340f10ff8070d623693bf0896584a82d9acfd3
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-12-31 18:16:42 +0000

    fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
-import Projects from "./sections/projects";
+import Projects from "./sections/Projects";
 import Footer from "./sections/Footer";
 import Experience from "./sections/Experience";
 
